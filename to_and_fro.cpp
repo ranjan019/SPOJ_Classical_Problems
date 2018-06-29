0761fd23fc1e378cb6b17bdf63d8e78354f5fc36
Ashutosh Ranjan <ashutosh.ranjan019@gmail.com>
2018-06-29 13:39:05 +0530

    Problem 902; very easy

---
+++ to_and_fro.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int i,j,k,l;
+
+	while(1)
+	{
+		int columns, rows, code_len;
+		cin>>columns;
+		if(columns==0)
+			break;
+
+		string code;
+		cin>>code;
+		code_len=code.size();
+		rows=code_len/columns;
+
+		for(i=0;i<columns;i++)
+		{
+			for(j=0;j<rows;j++)
+			{
+				if(j%2)
+				{
+					cout<<code[(j)*columns + columns - i -1];
+
+				}
+				else
+					cout<<code[j*columns + i];
+			}
+		}
+		cout<<endl;
+
+	}
+	return 0;
+}
